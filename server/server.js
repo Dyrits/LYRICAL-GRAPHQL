0d04f494f132ff524d6f34424a5f0478ed1ff902
Dylan J. Gerrits <dyrits@outlook.com>
2023-09-28 15:26:20 +0200

    Update MongoDB connection string and logs, add docker-compose.yml
    Updated MongoDB URI from an empty string to localhost in server.js. Enhanced connection logs to give more accurate and meaningful messages when connecting to MongoDB.
    
    Added docker-compose.yml for easier setup and running of MongoDB in a Docker container, allowing isolation and portability.
    
    Updated .gitignore to exclude Docker volume files created when using docker-compose, keeping the repository clean from generated files.

--- server/server.js
+++ server/server.js
@@ -1,38 +1,37 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 
 const app = express();
 
-// Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = "mongodb://localhost:27017/lyrical";
 if (!MONGO_URI) {
-  throw new Error('You must provide a Mongo Atlas URI');
+  throw new Error("You must provide a valid Mongo DB URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo Atlas instance.'))
-  .on('error', (error) =>
-    console.log('Error connecting to Mongo Atlas:', error)
+  .once("open", () => console.log("The connection to Mongo DB is open."))
+  .on("error", (error) =>
+    console.log("The connection to Mongo DB encountered an error.", error)
   );
 
 app.use(bodyParser.json());
 app.use(
-  '/graphql',
+  "/graphql",
   expressGraphQL({
     schema,
     graphiql: true
   })
 );
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
